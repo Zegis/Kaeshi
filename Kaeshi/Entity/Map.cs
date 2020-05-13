522b75c39873bee58be1ad0763a9023465ac9713
zegis <kjgm@poczta.onet.pl>
2020-05-13 17:42:06 +0200

    Added map entity

---
+++ Kaeshi/Entity/Map.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keshi.Entity
+{
+    class Map
+    {
+        private readonly Dictionary<string, Location> locations;
+        private Location current;
+
+        public Map()
+        {
+            locations = LoadLocations();
+        }
+
+        private Dictionary<string, Location> LoadLocations()
+        {
+            var locations = new Dictionary<string, Location>();
+            locations.Add("root", new Location());
+
+            current = locations["root"];
+
+            return locations;
+        }
+
+        public Location GetCurrentLocation()
+        {
+            return current;
+        }
+    }
+}
---
+++ Kaeshi/Interfaces/IVisitable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keshi.Interfaces
+{
+    public interface IVisitable
+    {
+        public void Go(string direction);
+    }
+}
