94933a779995ba92645fc1b55bf98dff31524c4b
zegis <kjgm@poczta.onet.pl>
2020-10-03 01:32:40 +0200

    Added NPCs to locations, add ability to look at them

--- Kaeshi/Entity/Location.cs
+++ Kaeshi/Entity/Location.cs
@@ -1,6 +1,7 @@
 ﻿using Keshi.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Keshi.Entity
@@ -13,10 +14,14 @@ namespace Keshi.Entity
 
         private string secondaryDescription;
 
-        private Dictionary<Direction, Location> linkedLoctions = new Dictionary<Direction, Location>();
+        private Dictionary<string, Character> npcs;
+
+        private Dictionary<Direction, Location> linkedLoctions;
 
         public Location(string firstDescription, string secondDescription)
         {
+            linkedLoctions = new Dictionary<Direction, Location>();
+            npcs = new Dictionary<string, Character>();
             primaryDescription = firstDescription;
             secondaryDescription = secondDescription;
             visited = false;
@@ -25,10 +30,21 @@ namespace Keshi.Entity
         public string Observe()
         {
             if (visited)
-                return secondaryDescription;
+                return secondaryDescription + " " + String.Join("", npcs.Keys);
             else
-                return primaryDescription;
+                return primaryDescription + " " + String.Join("", npcs.Keys);
             
+        }
+
+        public IVisible ObserveNpc(string NpcName)
+        {
+            npcs.TryGetValue(NpcName, out var retVar);
+            return retVar;
+        }
+
+        public void AddNpc(string NpcName ,Character NpcToAdd)
+        {
+            npcs[NpcName] = NpcToAdd;
         }
 
         public void SetLink(Direction direction, Location locationToLink)
