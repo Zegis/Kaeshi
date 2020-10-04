af171afd8516a6086de2904d594677345a00113a
zegis <kjgm@poczta.onet.pl>
2020-10-04 10:36:33 +0200

    Added dice-throw module

---
+++ Kaeshi/Modules/Dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keshi.Modules
+{
+    public class Dice
+    {
+        private static readonly Random rng = new Random();
+
+        public static int Throw()
+        {
+            return rng.Next(1, 6);
+        }
+    }
+}
