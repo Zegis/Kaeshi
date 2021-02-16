86825f78f45df54238f7b81d4bf2b3fa30085392
zegis <kjgm@poczta.onet.pl>
2021-02-16 10:41:01 +0100

    Added unit tests for Not Found Command and Exit command

---
+++ Kaeshi.Tests/CommandsTests/SimpleCommandsTests.cs
@@ -0,0 +1,34 @@
+﻿using Keshi.Commands;
+using Keshi.Entity;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    class SimpleCommandsTests
+    {
+        [Test]
+        public void NotFoundCommandsReturnsPlayState()
+        {
+            GameState expected = GameState.Play;
+
+            var command = new NotFoundCommand();
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExitCommandsReturnsExitState()
+        {
+            GameState expected = GameState.Exit;
+
+            var command = new ExitCommand();
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
