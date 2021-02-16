de8a88509ac1481f42fef09ef2d61bdb1693e992
zegis <kjgm@poczta.onet.pl>
2021-02-16 17:19:09 +0100

    Added simple go command test

---
+++ Kaeshi.Tests/CommandsTests/GoCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using Keshi.Commands;
+using Keshi.Entity;
+using NUnit.Framework;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    class GoCommandTests
+    {
+        [Test]
+        public void GoCommandReturnsPlay()
+        {
+            GameState expected = GameState.Play;
+            Map map = new Map();
+
+            var command = new GoCommand(map, Direction.North);
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Kaeshi.Tests/CommandsTests/SimpleCommandsTests.cs
+++ Kaeshi.Tests/CommandsTests/SimpleCommandsTests.cs
@@ -1,9 +1,6 @@
 ﻿using Keshi.Commands;
 using Keshi.Entity;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Kaeshi.Tests.CommandsTests
 {
