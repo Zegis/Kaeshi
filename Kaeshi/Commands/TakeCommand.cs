f57a11d42638b03861095e566ac023d0a7383d4a
Zegis <kjgm@poczta.onet.pl>
2021-04-03 18:07:30 +0200

    Added Take Command, and backpack interface for equipment manipulation

---
+++ Kaeshi/Commands/TakeCommand.cs
@@ -0,0 +1,23 @@
+﻿using Kaeshi.Entity;
+using Kaeshi.Interfaces;
+
+namespace Kaeshi.Commands
+{
+    public class TakeCommand : ICommand
+    {
+        private readonly IBackpack backpack;
+        private readonly Item item;
+
+        public TakeCommand(IBackpack characterBackpack, Item itemToTake)
+        {
+            backpack = characterBackpack;
+            item = itemToTake;
+        }
+
+        public GameState Execute()
+        {
+            backpack.PutInBackpack(item);
+            return GameState.Play;
+        }
+    }
+}
---
+++ Kaeshi/Interfaces/IBackpack.cs
@@ -0,0 +1,10 @@
+﻿using Kaeshi.Entity;
+
+namespace Kaeshi.Interfaces
+{
+    public interface IBackpack
+    {
+        void PutInBackpack(Item item);
+        void RemoveFromBackpack(Item item);
+    }
+}
