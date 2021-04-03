6f82f2d1239d1a279bec32e7ed68642c4591d1cc
Zegis <kjgm@poczta.onet.pl>
2021-04-03 14:32:09 +0200

    Added name and descrption fields for item class

--- Kaeshi/Entity/Item.cs
+++ Kaeshi/Entity/Item.cs
@@ -2,5 +2,21 @@
 {
     public class Item
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Item()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+        }
+
+        public Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
     }
 }
