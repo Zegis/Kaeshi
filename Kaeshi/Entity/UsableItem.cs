4049d613a279e80c0e2e1c9f9785d1de96b482e6
zegis <kjgm@poczta.onet.pl>
2022-01-08 16:25:35 +0100

    Added Usable items interface and derieved class

---
+++ Kaeshi/Entity/UsableItem.cs
@@ -0,0 +1,32 @@
+﻿using Kaeshi.Entity;
+using Keshi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keshi.Entity
+{
+    public class UsableItem : Item, IUsable
+    {
+        private int _usesRemaining;
+
+        UsableItem(string name, string description, int maxUses): base(name, description)
+        {
+            _usesRemaining = maxUses;
+        }
+
+        public void Use()
+        {
+            if (_usesRemaining == 0)
+                Console.Write("Item is depleted");
+
+            _usesRemaining--;
+            Console.Write($"Used {Name}");
+        }
+
+        public override string Observe()
+        {
+            return $"{Name}\r\n{Description}\r\nUses Left: {_usesRemaining}";
+        }
+    }
+}
---
+++ Kaeshi/Interfaces/IUsable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keshi.Interfaces
+{
+    public interface IUsable
+    {
+        void Use();
+    }
+}
