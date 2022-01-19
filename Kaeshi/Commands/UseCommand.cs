e0b5ccf312dac9a09321ffb51a5f93301a224e4a
zegis <kjgm@poczta.onet.pl>
2022-01-19 23:54:39 +0100

    Added use command

---
+++ Kaeshi/Commands/UseCommand.cs
@@ -0,0 +1,32 @@
+﻿using Kaeshi.Entity;
+using Kaeshi.Interfaces;
+using System;
+
+namespace Kaeshi.Commands
+{
+    public class UseCommand : ICommand
+    {
+        IBattler _target;
+        UsableItem _item;
+
+        public UseCommand(UsableItem itemToUse,IBattler target)
+        {
+            _item = itemToUse;
+            _target = target;
+        }
+
+        public GameState Execute()
+        {
+            if(_item == null)
+            {
+                Console.WriteLine("Nothing to use...");
+            }
+            else
+            {
+                _item.Use(_target);
+            }
+
+            return GameState.Play;
+        }
+    }
+}
