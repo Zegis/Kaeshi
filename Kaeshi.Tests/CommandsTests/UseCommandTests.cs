0b78cfef6da9b3e627b33097d4353d7e9ad1732c
zegis <kjgm@poczta.onet.pl>
2022-01-22 02:00:07 +0100

    Add simple UseCommandTest

---
+++ Kaeshi.Tests/CommandsTests/UseCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Moq;
+using Kaeshi.Entity;
+using Kaeshi.Commands;
+using Kaeshi.Interfaces;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    public class UseCommandTests
+    {
+        [Test]
+        public void UseCommandReturnsPlay()
+        {
+            var expected = GameState.Play;
+
+            var target = new Mock<IBattler>();
+
+            var command = new UseCommand(null, target.Object);
+
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
