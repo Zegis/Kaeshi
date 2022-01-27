8d683dbd2a27abb3044d8044142bec949f39eea9
zegis <kjgm@poczta.onet.pl>
2022-01-27 17:03:39 +0100

    Expanded tests if UseCommand returns proper game state

--- Kaeshi.Tests/CommandsTests/UseCommandTests.cs
+++ Kaeshi.Tests/CommandsTests/UseCommandTests.cs
@@ -9,13 +9,29 @@ namespace Kaeshi.Tests.CommandsTests
     public class UseCommandTests
     {
         [Test]
-        public void UseCommandReturnsPlay()
+        public void UseCommandReturnsPlayWithNullItem()
         {
             var expected = GameState.Play;
 
             var target = new Mock<IBattler>();
 
             var command = new UseCommand(null, target.Object);
+
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void UseCommandReturnsPlayWithItem()
+        {
+            var expected = GameState.Play;
+
+            var target = new Mock<IBattler>();
+
+            var item = new UsableItem("foo", "bar", 1, (x => x.IsAlive()));
+
+            var command = new UseCommand(item, target.Object);
 
             var actual = command.Execute();
 
