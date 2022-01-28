2d90f3e45be584609c9d6cea4cacb8e0ef29ff11
zegis <kjgm@poczta.onet.pl>
2022-01-28 17:58:14 +0100

    Created effectLibrary module to store list of all usable items effects possible

---
+++ Kaeshi/Modules/EffectsLibrary.cs
@@ -0,0 +1,37 @@
+﻿using Kaeshi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaeshi.Modules
+{
+    public static class EffectsLibrary
+    {
+        static readonly private Dictionary<string, Action<IBattler>> _effects;
+
+        static EffectsLibrary()
+        {
+            _effects = new Dictionary<string, Action<IBattler>>
+            {
+                ["heal"] = (a) => a.Heal(2),
+                ["damage"] = (a) => a.Injure(2)
+            };
+        }
+
+        static public Action<IBattler> Get(string effectName)
+        {
+            try
+            {
+                return _effects[effectName];
+            }
+            catch(ArgumentNullException)
+            {
+                return null;
+            }
+            catch(KeyNotFoundException)
+            {
+                return null;
+            }
+        }
+    }
+}
