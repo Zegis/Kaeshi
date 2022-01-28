cfd0ec5d02c7b44f66727b269d33dd3b9c12efe1
zegis <kjgm@poczta.onet.pl>
2022-01-28 18:25:33 +0100

    Added EquipableItem class

---
+++ Kaeshi/Entity/Items/EquipableItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaeshi.Entity
+{
+    public class EquipableItem: Item
+    {
+        public readonly int _modificator;
+
+        public EquipableItem(string name, string description, int modificator) : base(name, description)
+        {
+            _modificator = modificator;
+        }
+
+        public int GetModificator()
+        {
+            return _modificator;
+        }
+    }
+}
