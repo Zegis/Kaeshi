b984ca7e1bff20a712dbff836262924b52e3bfa2
zegis <kjgm@poczta.onet.pl>
2022-01-31 01:33:25 +0100

    Fixed literal in Equippable names. Added IEquipment interface

---
+++ Kaeshi/Interfaces/IEquipment.cs
@@ -0,0 +1,10 @@
+﻿using Kaeshi.Entity;
+
+namespace Kaeshi.Interfaces
+{
+    interface IEquipment
+    {
+        void Equip(EquippableItem item, EquippableType type);
+        void Unequip(EquippableType type);
+    }
+}
