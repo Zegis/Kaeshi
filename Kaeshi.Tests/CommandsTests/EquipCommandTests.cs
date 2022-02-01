e34ffd2c13d8fac2b4a184ade78e00c621651d7f
zegis <kjgm@poczta.onet.pl>
2022-02-02 00:46:32 +0100

    Added EquipCommandTests

---
+++ Kaeshi.Tests/CommandsTests/EquipCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using Kaeshi.Commands;
+using Kaeshi.Entity;
+using Kaeshi.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    class EquipCommandTests
+    {
+        [Test]
+        public void UseCommandReturnsPlayWithNullItem()
+        {
+            var expected = GameState.Play;
+
+            var target = new Mock<IEquipment>();
+
+            var command = new EquipCommand(target.Object,null);
+
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void UseCommandReturnsPlayWithItem()
+        {
+            var expected = GameState.Play;
+
+            var target = new Mock<IEquipment>();
+
+            var item = new EquippableItem("foo", "bar", 1, EquippableType.Armor);
+
+            var command = new EquipCommand(target.Object, item);
+
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
