2d25f25e68546a2444cb77a8bc092c94a70564bf
zegis <kjgm@poczta.onet.pl>
2022-02-03 17:37:25 +0100

    Added CharacterEquipment Tests

---
+++ Kaeshi.Tests/CharacterEquipmentTests.cs
@@ -0,0 +1,45 @@
+﻿using Kaeshi.Entity;
+using NUnit.Framework;
+
+namespace Kaeshi.Tests
+{
+    class CharacterEquipmentTests
+    {
+        [Test]
+        public void CanEquipItemToEmptySpace()
+        {
+            var sut = new Character(1, 1, 1);
+            var item = new EquippableItem("foo", "bar", 0, EquippableType.Armor);
+            sut.PutInBackpack(item);
+
+            sut.Equip(item, EquippableType.Armor);
+
+            Assert.IsNotNull(sut.Equipment[EquippableType.Armor]);
+        }
+
+        [Test]
+        public void CanNotEquipItemToFilledSpace()
+        {
+            var sut = new Character(1, 1, 1);
+            sut.Equipment[EquippableType.Armor] = new EquippableItem("bar", "foo", 0, EquippableType.Armor);
+            var item = new EquippableItem("foo", "bar", 0, EquippableType.Armor);
+            sut.PutInBackpack(item);
+
+            sut.Equip(item, EquippableType.Armor);
+
+            Assert.AreNotEqual(sut.Equipment[EquippableType.Armor].Observe(),item.Observe());
+        }
+
+        [Test]
+        public void EquipedItemIsNotInBackpack()
+        {
+            var sut = new Character(1, 1, 1);
+            var item = new EquippableItem("foo", "bar", 0, EquippableType.Armor);
+            sut.PutInBackpack(item);
+
+            sut.Equip(item, EquippableType.Armor);
+
+            Assert.IsNull(sut.GetEquippableItem(item.Name));
+        }
+    }
+}
