a89aad9b6427d04bb9912551bd821776b980261b
zegis <kjgm@poczta.onet.pl>
2022-02-03 23:36:06 +0100

    Added UnEquip Command test

---
+++ Kaeshi.Tests/CommandsTests/UnEquipCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using Kaeshi.Commands;
+using Kaeshi.Entity;
+using NUnit.Framework;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    class UnEquipCommandTests
+    {
+        [Test]
+        public void UnEquipCommandReturnsPlay()
+        {
+            var expected = GameState.Play;
+            var command = new UnequipCommand();
+
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
