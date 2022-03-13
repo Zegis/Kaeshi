e48e0e014bffbce0cb763e951c5b82fcd2333aaa
Zegis <kjgm@poczta.onet.pl>
2022-03-13 23:15:23 +0100

    Added simple status test for OpenIntentoryCommand

---
+++ Kaeshi.Tests/CommandsTests/OpenInventoryCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Kaeshi.Commands;
+using Kaeshi.Entity;
+using Kaeshi.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    class OpenInventoryCommandTests
+    {
+        [Test]
+        public void OpenInventoryCommandReturnsPlay()
+        {
+            var expected = GameState.Play;
+
+            var character = new Mock<IBackpack>();
+            var command = new OpenInventoryCommand(character.Object);
+
+            var actual = command.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
