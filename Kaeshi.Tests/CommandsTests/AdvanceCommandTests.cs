2eed6a9a9dadd8174b8cff5c8510ec717f6eda4d
Zegis <kjgm@poczta.onet.pl>
2025-07-24 11:01:31 +0200

    Added tests suite for AdvanceCommandTest with single test

---
+++ Kaeshi.Tests/CommandsTests/AdvanceCommandTests.cs
@@ -0,0 +1,34 @@
+﻿using Kaeshi.Commands;
+using Kaeshi.Entity;
+using Kaeshi.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Kaeshi.Tests.CommandsTests
+{
+    public class AdvanceCommandTests
+    {
+        [Test]
+        public void AdvanceCommandCantAdvanceOnRegularLocation()
+        {
+            var map = new Mock<IMap>();
+            var location = new Location("foo", "bar");
+            map.Setup(m => m.GetCurrentLocation()).Returns(location);
+            var backpack = new Mock<IBackpack>();
+            var command = new AdvanceCommand(map.Object, backpack.Object);
+
+            using(StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                var result = command.Execute();
+
+                Console.SetOut(Console.Out);
+
+                Assert.AreEqual("There's no way to next floor here", sw.ToString().Trim());
+                Assert.AreEqual(GameState.Play, result);
+            }
+        }
+    }
+}
