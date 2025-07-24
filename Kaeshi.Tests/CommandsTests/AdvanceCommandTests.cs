fb4f9c7fa8b86d5cdbe0cdc069777e8ccb68caa9
Zegis <kjgm@poczta.onet.pl>
2025-07-24 11:19:24 +0200

    Added rest of AdvanceCommand tests

--- Kaeshi.Tests/CommandsTests/AdvanceCommandTests.cs
+++ Kaeshi.Tests/CommandsTests/AdvanceCommandTests.cs
@@ -8,10 +8,11 @@ using System.IO;
 
 namespace Kaeshi.Tests.CommandsTests
 {
+    [TestOf(typeof(AdvanceCommand))]
     public class AdvanceCommandTests
     {
         [Test]
-        public void AdvanceCommandCantAdvanceOnRegularLocation()
+        public void AdvanceCommandCanNotAdvanceOnRegularLocation()
         {
             var map = new Mock<IMap>();
             var location = new Location("foo", "bar");
@@ -26,7 +27,51 @@ namespace Kaeshi.Tests.CommandsTests
 
                 Console.SetOut(Console.Out);
 
+                map.Verify(m => m.Advance(), Times.Never);
                 Assert.AreEqual("There's no way to next floor here", sw.ToString().Trim());
+                Assert.AreEqual(GameState.Play, result);
+            }
+        }
+
+        [Test]
+        public void AdvanceCommandCanAdvanceOnFinalLocationWithKey()
+        {
+            var map = new Mock<IMap>();
+            var location = new Location("foo", "bar");
+            location.SetFinalLocation("key");
+            map.Setup(m => m.GetCurrentLocation()).Returns(location);
+            var backpack = new Mock<IBackpack>();
+            backpack.Setup(b => b.ItemInBackpack(It.IsAny<string>())).Returns(true);
+            var command = new AdvanceCommand(map.Object, backpack.Object);
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                var result = command.Execute();
+                Console.SetOut(Console.Out);
+                map.Verify(m => m.Advance(), Times.Once);
+                Assert.AreEqual("You reached next floor!", sw.ToString().Trim());
+                Assert.AreEqual(GameState.Play, result);
+            }
+        }
+
+        [Test]
+        public void AdvanceCommandCanNotAdvanceOnFinalLocationWithoutKey()
+        {
+            var keyName = "key";
+            var map = new Mock<IMap>();
+            var location = new Location("foo", "bar");
+            location.SetFinalLocation(keyName);
+            map.Setup(m => m.GetCurrentLocation()).Returns(location);
+            var backpack = new Mock<IBackpack>();
+            backpack.Setup(b => b.ItemInBackpack(It.IsAny<string>())).Returns(false);
+            var command = new AdvanceCommand(map.Object, backpack.Object);
+            using(StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                var result = command.Execute();
+                Console.SetOut(Console.Out);
+                map.Verify(m => m.Advance(), Times.Never);
+                Assert.AreEqual($"You don't have a {keyName} to advance!", sw.ToString().Trim());
                 Assert.AreEqual(GameState.Play, result);
             }
         }
